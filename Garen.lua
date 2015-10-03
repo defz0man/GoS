a3a563b41b5a79056a896634dc601a4c1f56dcd5
LoggeL <hyper.xjo@gmx.de>
2015-10-03 21:24:32 +0200

    Update Garen.lua
    Removed 1 base dmg (safety)

--- Garen.lua
+++ Garen.lua
@@ -6,9 +6,9 @@ OnLoop (function (myHero)
 	local target = GetCurrentTarget()
 	
 	if CanUseSpell(myHero,_R) == READY and GoS:ValidTarget(target, GetCastRange(myHero,_R)) then
-		if GotBuff(target,"garenpassivegrit")>1 and GetCurrentHP(target) < GoS:CalcDamage(myHero, target, 0, 175*GetCastLevel(myHero,_R) + ((GetMaxHP(target)-GetCurrentHP(target))*(0.219+0.067*GetCastLevel(myHero, _R)))) then
+		if GotBuff(target,"garenpassivegrit")>1 and GetCurrentHP(target) < GoS:CalcDamage(myHero, target, 0, 174*GetCastLevel(myHero,_R) + ((GetMaxHP(target)-GetCurrentHP(target))*(0.219+0.067*GetCastLevel(myHero, _R)))) then
 			CastTargetSpell(target,_R)
-		elseif GotBuff(target,"garenpassivegrit")<1 and GetCurrentHP(target) < (175*GetCastLevel(myHero,_R) + ((GetMaxHP(target)-GetCurrentHP(target))*(0.219+0.067*GetCastLevel(myHero, _R)))) then
+		elseif GotBuff(target,"garenpassivegrit")<1 and GetCurrentHP(target) < (174*GetCastLevel(myHero,_R) + ((GetMaxHP(target)-GetCurrentHP(target))*(0.219+0.067*GetCastLevel(myHero, _R)))) then
 			CastTargetSpell(target,_R)
 		end
 	end	
