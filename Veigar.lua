839b2f3d9d471f512564951d7273688146d5dbf6
LoggeL <hyper.xjo@gmx.de>
2015-10-12 20:31:35 +0200

    Update Veigar.lua
    E Logic <3 Deftsu

--- Veigar.lua
+++ Veigar.lua
@@ -25,11 +25,10 @@ local myHero=GetMyHero()
 OnLoop(function (myHero)
 	if not IsDead(myHero) then
 	local unit=GetCurrentTarget()
-		KS(unit)
+		KS()
 		AutoW(unit)
 		Combo(unit)
 		FarmQ()
-		drawDmg(unit)
 	end
 end)
 
@@ -57,49 +56,52 @@ function Combo(unit)
 				CastSkillShot(_W,WPred.PredPos.x,WPred.PredPos.y,WPred.PredPos.z)
 			end
 		end	
-		
-		if Config.c.E:Value() and CanUseSpell(myHero,_E) == READY and GoS:ValidTarget(unit, GetCastRange(myHero,_E)) then
-			local EPred = GetPredictionForPlayer(GetOrigin(myHero),unit,GetMoveSpeed(unit),math.huge,GetCastRange(myHero,_E),550,80,false,false)
-			if EPred.HitChance == 1 then				
-				CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
+		castE(unit)
+	end
+end
+
+function KS()
+	for _,unit in pairs(Gos:GetEnemyHeroes()) do
+		if Config.c.AR:Value() and CanUseSpell(myHero,_R) == READY and GoS:ValidTarget(unit, GetCastRange(myHero,_R)) then
+			local RPercent=GetCurrentHP(unit)/GoS:CalcDamage(myHero, unit, 0, (125*GetCastLevel(myHero,_R)+GetBonusAP(myHero)+125+GetBonusAP(unit)*0.8))
+			if RPercent<1 and RPercent>0.2 then 
+				CastTargetSpell(unit,_R)
 			end
+		end	
+		if Config.m.D:Value() and CanUseSpell(myHero,_R)==READY and GoS:ValidTarget(unit,1500) then
+			local unit=GetCurrentTarget()
+			local RDmg=GoS:CalcDamage(myHero, unit, 0, (125*GetCastLevel(myHero,_R) + 125 + (GetBonusAP(myHero) + 0.8*(GetBonusAP(unit)))))
+			if RDmg>=GetCurrentHP(unit) then
+				RDmg=GetCurrentHP(unit)
+			end
+		DrawDmgOverHpBar(unit,GetCurrentHP(unit),0,RDmg,0xffffffff)
 		end
 	end
 end
 
-function KS(unit)
-	if Config.c.AR:Value() and CanUseSpell(myHero,_R) == READY and GoS:ValidTarget(unit, GetCastRange(myHero,_R)) then
-		local RPercent=GetCurrentHP(unit)/GoS:CalcDamage(myHero, unit, 0, (125*GetCastLevel(myHero,_R)+GetBonusAP(myHero)+125+GetBonusAP(unit)*0.8))
-		if RPercent<1 and RPercent>0.2 then 
-			CastTargetSpell(unit,_R)
-		end
-	end	
-end
-
 function AutoW(unit)
 	if Config.c.AW:Value() and GoS:ValidTarget(unit,GetCastRange(myHero,_W)) and GotBuff(unit, "veigareventhorizonstun") > 0 and (GotBuff(unit, "snare") > 0 or GotBuff(unit, "taunt") > 0 or GotBuff(unit, "suppression") > 0 or GotBuff(unit, "stun")) then
-	local WPred = GetPredictionForPlayer(GetOrigin(myHero),unit,GetMoveSpeed(unit),math.huge,GetCastRange(myHero,_W),550,80,false,false)
+	local WPred = GetPredictionForPlayer(GetOrigin(myHero),unit,GetMoveSpeed(unit),math.huge,550,GetCastRange(myHero,_W),80,false,false)
 		if WPred.HitChance == 1 then
 			CastSkillShot(_W,WPred.PredPos.x,WPred.PredPos.y,WPred.PredPos.z)
 		end
 	end
 end
 
-function castE()
-end
-
-function drawDmg(unit)
-	if Config.m.D:Value() and CanUseSpell(myHero,_R)==READY and GoS:ValidTarget(unit,1000) then
-		local unit=GetCurrentTarget()
-		local RDmg=GoS:CalcDamage(myHero, unit, 0, (125*GetCastLevel(myHero,_R) + 125 + (GetBonusAP(myHero) + 0.8*(GetBonusAP(unit)))))
-		DrawDmgOverHpBar(unit,GetCurrentHP(unit),0,RDmg,0xffffffff)
+function castE(unit)
+	if Config.c.E:Value() and CanUseSpell(myHero,_E) == READY and GoS:ValidTarget(unit, GetCastRange(myHero,_E)) then
+		local targetPos=GetOrigin(unit)
+		local EPred = GetPredictionForPlayer(GetOrigin(myHero),unit,GetMoveSpeed(unit),math.huge,650,GetCastRange(myHero,_E),80,false,false)
+		EPred.PredPos=Vector(EPred.PredPos)+((GetOrigin(myHero)-Vector(EPred.PredPos)):normalized()*350)
+		CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
 	end
 end
+
 
 function FarmQ()
 	if Config.f.AQ:Value() and CanUseSpell(myHero,_Q)==READY and IOW:Mode() ~= "Combo" then
 		for i,creep in pairs(GoS:GetAllMinions(MINION_ENEMY)) do
-			if GoS:ValidTarget(creep,GetCastRange(myHero,_Q)) and GetCurrentHP(creep)<GoS:CalcDamage(myHero, creep, 0, (45*GetCastLevel(myHero,_Q)+30+GetBonusAP(myHero)*0.6)) then
+			if GoS:ValidTarget(creep,GetCastRange(myHero,_Q)) and GetCurrentHP(creep)<GoS:CalcDamage(myHero, creep, 0, (45*GetCastLevel(myHero,_Q)+25+GetBonusAP(myHero)*0.6)) then
 				CreepOrigin=GetOrigin(creep)
 				if Config.m.D:Value() then	DrawCircle(CreepOrigin.x,CreepOrigin.y,CreepOrigin.z,75,0,3,0xffffff00) end
 				QCol=Collision(GetCastRange(myHero,_Q),1200,925,70)
