24e1408f387455c86dbfbf91ef6f36ceece370b0
LoggeL <hyper.xjo@gmx.de>
2015-10-21 15:05:51 +0200

    Update VelKoz.lua

--- VelKoz.lua
+++ VelKoz.lua
@@ -23,9 +23,9 @@ DegreeTable={45,-45,22.5,-22.5}
 
 function Combo(unit)
 	for _,i in pairs(GoS:GetEnemyHeroes()) do
-		if Config.c.Q:Value() and GetCastName(myHero,_Q)=="VelkozQ" and IOW:Mode() == "Combo" then
+		if Config.c.Q:Value() and GetCastName(myHero,_Q)=="VelkozQ" then
 
-			if GoS:ValidTarget(i,1500) then
+			if GoS:ValidTarget(i,1500) and IOW:Mode() == "Combo" then
 					
 				EnemyPos=GetOrigin(i)
 				DrawCircle(EnemyPos.x,EnemyPos.y,EnemyPos.z,750,0,3,0xffffff00)		
@@ -68,22 +68,21 @@ function Combo(unit)
 						QStart=GetOrigin(myHero)
 					end
 				end
-				
-				
+			end	
+		end
 			--if q traveling
-			elseif GetCastName(myHero,_Q)~="VelkozQ" and GoS:ValidTarget(i,2000) then
-				local split=QPred(GetOrigin(QBall),i)
-				local BallPos=GetOrigin(QBall)
-				--DrawCircle(BallPos.x,BallPos.y,BallPos.z,50,0,3,0xffffff00)
-				--DrawCircle(split.PredPos.x,split.PredPos.y,split.PredPos.z,50,0,3,0xffffff00)
-				--print(((Vector(GetOrigin(QBall))-Vector(QStart)):normalized()*(Vector(GetOrigin(QBall))-Vector(split.PredPos)):normalized())^2)
-				if split.HitChance==1 and ((Vector(GetOrigin(QBall))-Vector(QStart)):normalized()*(Vector(GetOrigin(QBall))-Vector(split.PredPos)):normalized())^2<0.01 then
-					--print("SPLIT")
-					CastSpell(_Q)
-				end
+		if GetCastName(myHero,_Q)~="VelkozQ" and GoS:ValidTarget(i,2000) then
+			local split=QPred(GetOrigin(QBall),i)
+			local BallPos=GetOrigin(QBall)
+			--DrawCircle(BallPos.x,BallPos.y,BallPos.z,50,0,3,0xffffff00)
+			--DrawCircle(split.PredPos.x,split.PredPos.y,split.PredPos.z,50,0,3,0xffffff00)
+			--print(((Vector(GetOrigin(QBall))-Vector(QStart)):normalized()*(Vector(GetOrigin(QBall))-Vector(split.PredPos)):normalized())^2)
+			if split.HitChance==1 and ((Vector(GetOrigin(QBall))-Vector(QStart)):normalized()*(Vector(GetOrigin(QBall))-Vector(split.PredPos)):normalized())^2<0.01 then
+				--print("SPLIT")
+				CastSpell(_Q)
 			end
 		end
-		
+				
 		if Config.c.W:Value() and IOW:Mode() == "Combo" then
 			if CanUseSpell(myHero, _W) == READY and GoS:ValidTarget(unit,GetCastRange(myHero,_W)) then
 			local WPred = GetPredictionForPlayer(GoS:myHeroPos(),unit,GetMoveSpeed(unit)*1.5,1000,250,GetCastRange(myHero,_W),50,false,true)
