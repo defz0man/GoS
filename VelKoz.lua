7603e910ed1d6d0f47f9d1c476ae3d2ada0fcb2e
LoggeL <hyper.xjo@gmx.de>
2015-10-30 17:53:54 +0100

    Update VelKoz.lua
    :)

--- VelKoz.lua
+++ VelKoz.lua
@@ -1,5 +1,5 @@
 require("Inspired")
-require("IOW")
+require("Collision")
 
 local Config=Menu("Vel","Vel")
 Config:SubMenu("c", "Combo")
@@ -7,10 +7,9 @@ Config.c:Boolean("Q","Use Q",true)
 Config.c:Boolean("W","Use W",true)
 Config.c:Boolean("E","Use E",true)
 
-OnLoop(function(myHero)
+OnTick(function(myHero)
 	if not IsDead(myHero) then
-		local unit = GoS:GetTarget(1500, DAMAGE_MAGIC)		
-		myHeroPos=GetOrigin(myHero)
+		local unit = GetCurrentTarget()
 		Combo(unit)
 	end
 end)
@@ -22,10 +21,10 @@ DegreeTable={22.5,-22.5,45,-45}
 
 
 function Combo(unit)
-	for _,i in pairs(GoS:GetEnemyHeroes()) do
+	for _,i in pairs(GetEnemyHeroes()) do
 		if Config.c.Q:Value() and GetCastName(myHero,_Q)=="VelkozQ" then
 
-			if GoS:ValidTarget(i,1500) and IOW:Mode() == "Combo" then
+			if ValidTarget(i,1500) and IOW:Mode() == "Combo" then
 					
 				EnemyPos=GetOrigin(i)
 				DrawCircle(EnemyPos.x,EnemyPos.y,EnemyPos.z,750,0,3,0xffffff00)		
@@ -40,12 +39,21 @@ function Combo(unit)
 				local BVec=Vector(GetOrigin(myHero))-Vector(GetOrigin(i))
 					
 				for l=1,value,1 do
+				
 					--Degree Vector from table
 					local sideVec=getVec(BVec,DegreeTable[l]):normalized()*750
 						
 					--Part on the range around enemy
 					local circlespot=Vector(GetOrigin(i))+Vector(sideVec)
-						
+					
+					
+					QCol=Collision(1500,1200,925,70)
+					local state,Objects=QCol:__GetMinionCollision(myHero,circlespot,ENEMY)
+					local hitcount=0
+				
+					for i,unit in ipairs(Objects) do 
+						return
+					end
 					--Debug1
 					--DrawCircle(circlespot.x,circlespot.y,circlespot.z,75,0,3,0xffffff00)
 					
@@ -71,7 +79,7 @@ function Combo(unit)
 			end	
 		end
 			--if q traveling
-		if GetCastName(myHero,_Q)~="VelkozQ" and GoS:ValidTarget(i,2000) then
+		if GetCastName(myHero,_Q)~="VelkozQ" and ValidTarget(i,2000) then
 			local split=QPred(GetOrigin(QBall),i)
 			local BallPos=GetOrigin(QBall)
 			--DrawCircle(BallPos.x,BallPos.y,BallPos.z,50,0,3,0xffffff00)
@@ -84,15 +92,15 @@ function Combo(unit)
 		end
 				
 		if Config.c.W:Value() and IOW:Mode() == "Combo" then
-			if CanUseSpell(myHero, _W) == READY and GoS:ValidTarget(unit,GetCastRange(myHero,_W)) then
-			local WPred = GetPredictionForPlayer(GoS:myHeroPos(),unit,GetMoveSpeed(unit)*1.5,1000,250,GetCastRange(myHero,_W),70,false,true)
+			if CanUseSpell(myHero, _W) == READY and ValidTarget(unit,GetCastRange(myHero,_W)) then
+			local WPred = GetPredictionForPlayer(GetOrigin(myHero),unit,GetMoveSpeed(unit)*1.5,1000,250,GetCastRange(myHero,_W),70,false,true)
 			   CastSkillShot(_W,WPred.PredPos.x,WPred.PredPos.y,WPred.PredPos.z)
 			end
 		end
 		
 		if Config.c.E:Value() and IOW:Mode() == "Combo" then
-			if CanUseSpell(myHero, _E) == READY and GoS:ValidTarget(unit,GetCastRange(myHero,_E)) then
-			local EPred = GetPredictionForPlayer(GoS:myHeroPos(),unit,GetMoveSpeed(unit),1300,100,GetCastRange(myHero,_E),70,false,true)
+			if CanUseSpell(myHero, _E) == READY and ValidTarget(unit,GetCastRange(myHero,_E)) then
+			local EPred = GetPredictionForPlayer(GetOrigin(myHero),unit,GetMoveSpeed(unit),1300,100,GetCastRange(myHero,_E),70,false,true)
 			   CastSkillShot(_E,EPred.PredPos.x,EPred.PredPos.y,EPred.PredPos.z)
 			end
 		end
