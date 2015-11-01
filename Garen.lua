1068761daea5a5ae8268b14fb5749e015c0d4a02
LoggeL <hyper.xjo@gmx.de>
2015-11-01 15:33:46 +0100

    Update Garen.lua
    Rework

--- Garen.lua
+++ Garen.lua
@@ -1,3 +1,5 @@
+if GetObjectName(myHero) ~= "Garen" then return end
+
 require('Inspired')
 
 Config = Menu("Garen", "Garen")
@@ -6,32 +8,44 @@ Config.c:Boolean("R", "Use R", true)
 Config.c:Boolean("D", "Draw RDmg", true)
 
 OnTick (function (myHero)
+	for _,target in pairs(GetEnemyHeroes()) do		
+		if Config.c.R:Value() and CanUseSpell(myHero,_R) == READY and ValidTarget(target, GetCastRange(myHero,_R)) then
+			if GetCurrentHP(target)+GetDmgShield(target) < GetRDmg(target) then
+				CastTargetSpell(target,_R)
+			end
+		end	
+	end
+end)
+
+OnDraw (function (myHero)
 	for _,target in pairs(GetEnemyHeroes()) do
-	
+		if Config.c.D:Value() and CanUseSpell(myHero,_R) == READY and ValidTarget(target,1500) then
+		 bar=nil
+			if GetRDmg(target)>=GetCurrentHP(target) then
+				bar=GetCurrentHP(target)
+			else
+				bar=GetRDmg(target)
+			end
+			DrawDmgOverHpBar(target,GetCurrentHP(target),0,bar,0xffffffff)
+		end
+	end
+end)
+
+function passiveCheck(target)
 	if GotBuff(target,"garenpassivegrit")>0 then
 		local isMarked=true
 	elseif GotBuff(target,"garenpassivegrit")<1 then
 		local isMarked=false
 	end
-	
+	return isMarked
+end
+
+function GetRDmg(target)
 	local RDmg=174*GetCastLevel(myHero,_R) + ((GetMaxHP(target)-GetCurrentHP(target))*(0.219+0.067*GetCastLevel(myHero, _R)))
-	
-	if Config.c.D:Value() and CanUseSpell(myHero,_R) == READY and ValidTarget(target,1000) then
-		if isMarked then
-			DrawDmgOverHpBar(target,GetCurrentHP(target),0,RDmg,0xffffffff)
-		elseif not isMarked then
-			DrawDmgOverHpBar(target,GetCurrentHP(target),0,CalcDamage(myHero, target, 0, RDmg),0xffffffff)
-		end
+	if not isMarked then
+		RDmg=CalcDamage(myHero, target, 0, RDmg)
 	end
-	
-	if Config.c.R:Value() and CanUseSpell(myHero,_R) == READY and ValidTarget(target, GetCastRange(myHero,_R)) then
-		if not isMarked and GetCurrentHP(target)+GetDmgShield(target) < CalcDamage(myHero, target, 0, RDmg) then
-			CastTargetSpell(target,_R)
-		elseif isMarkend and GetCurrentHP(target)+GetDmgShield(target) < RDmg then
-			CastTargetSpell(target,_R)
-		end
-	end	
-	end
-end)
+	return RDmg
+end
 
 PrintChat("Garen Auto R - Logge")
