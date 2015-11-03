a4c5a996783c2485233e608e5fe6ecda52a3e389
LoggeL <hyper.xjo@gmx.de>
2015-11-03 01:10:45 +0100

    Update Soraka.lua
    Updated with Ready(slot)

--- Soraka.lua
+++ Soraka.lua
@@ -26,7 +26,7 @@ OnTick(function (myHero)
 end)
 
 function heals()
-	if CanUseSpell(myHero,_W) == READY or CanUseSpell(myHero,_R) == READY then
+	if Ready(_W) or Ready(_R) then
 		for _,champ in pairs(GetAllyHeroes()) do
 				if Config.h.AW:Value() then
 					if not IsDead(champ) and (GetCurrentHP(champ)/GetMaxHP(champ)<healvalue) and GetDistance(myHero,champ)<GetCastRange(myHero,_W) then
@@ -51,14 +51,14 @@ end
 
 function combo(unit)
 	if IOW:Mode() == "Combo" then
-		if Config.c.Q:Value() and CanUseSpell(myHero,_Q) == READY and ValidTarget(unit,GetCastRange(myHero,_Q)*0.85) then
+		if Config.c.Q:Value() and Ready(_Q) and ValidTarget(unit,GetCastRange(myHero,_Q)*0.85) then
 			local Delay=(GetDistance(myHero,unit)/GetCastRange(myHero,_Q))*750
 			local QPred = GetPredictionForPlayer(GetOrigin(myHero),unit,GetMoveSpeed(unit)-10,math.huge,GetCastRange(myHero,_Q),250+Delay,80,false,true)
 			if QPred.HitChance==1 then
 				CastSkillShot(_Q,QPred.PredPos.x,QPred.PredPos.y,QPred.PredPos.z)
 			end
 		end
-		if Config.c.E:Value() and CanUseSpell(myHero,_E) == READY and ValidTarget(unit,GetCastRange(myHero,_E)) then
+		if Config.c.E:Value() and Ready(_E) and ValidTarget(unit,GetCastRange(myHero,_E)) then
 			local EnemyOrg=GetOrigin(unit)
 			CastSkillShot(_E,EnemyOrg.x,EnemyOrg.y,EnemyOrg.z)
 		end
