94ed96fef70c06a4e756f7cadb864db856a88c63
LoggeL <hyper.xjo@gmx.de>
2016-01-06 22:13:05 +0100

    Update Varus.lua

--- Varus.lua
+++ Varus.lua
@@ -7,11 +7,14 @@ require("OpenPredict")
 VMenu = Menu("Varus", "Varus")
 VMenu:SubMenu("c", "Combo")
 VMenu.c:Boolean("Q", "Use Q", true)
+VMenu.c:Slider("sQ", "Stacks to use Q", 0, 0, 3, 1)
 VMenu.c:Boolean("E", "Use E", true)
+VMenu.c:Slider("sE", "Stacks to use E", 1, 0, 3, 1)
 VMenu.c:Boolean("R", "Use R", true)
 VMenu.c:Slider("RE", "Enemies around for R", 3, 1, 5, 1)
 
 VMenu:SubMenu("ks", "Killsteal")
+VMenu.ks:Boolean("KSS","Smart Killsteal", true)
 VMenu.ks:Boolean("KSQ","Killsteal with Q", true)
 VMenu.ks:Boolean("KSE","Killsteal with E", true)
 VMenu.ks:Boolean("KSR","Killsteal with R", true)
@@ -80,12 +83,12 @@ function combo(unit)
 	if qCharge and (1000 + (GetTickCount() - qTime) * 0.4) < 1625 then
 		qRange = 1000 + (GetTickCount() - qTime) * 0.4
 	elseif not qCharge then
-		qRange = 0
+		qRange = 900
 	end
 	
 	if IOW:Mode() == "Combo" then
 		--Q1
-		if VMenu.c.Q:Value() and Ready(_Q) and ValidTarget(unit, 1500) and not qCharge then
+		if VMenu.c.Q:Value() and Ready(_Q) and ValidTarget(unit, 1500) and not qCharge and wTrack[GetObjectName(unit)] >= VMenu.c.sQ:Value() then
 			CastSkillShot(_Q,GetOrigin(myHero))	
 		end
 		
@@ -101,7 +104,7 @@ function combo(unit)
 		end
 		
 		--E
-		if Ready(_E) and VMenu.c.E:Value() and ValidTarget(unit, GetCastRange(myHero,_E)) then
+		if Ready(_E) and VMenu.c.E:Value() and ValidTarget(unit, GetCastRange(myHero,_E)) and wTrack[GetObjectName(unit)] >= VMenu.c.sE:Value() then
 			local EPred=GetCircularAOEPrediction(unit, VarusE)
 			if EPred and EPred.hitChance >= (VMenu.p.hE:Value()/100) then
 				CastSkillShot(_E,EPred.castPos)
@@ -137,9 +140,30 @@ end)
 
 function ks()
 	for i,unit in pairs(GetEnemyHeroes()) do
-		if VMenu.ks.KSQ:Value() and Ready(_Q) and ValidTarget(unit,1500) and GetCurrentHP(unit)+GetDmgShield(unit) <  CalcDamage(myHero, unit, GetCastLevel(myHero,_Q)*50-40+GetBonusDmg(unit)*1.5 ,0) + StackDamage(unit) then 
+		
+		--Smark KS
+		if VMenu.ks.KSS:Value() and ValidTarget(unit, 1075) and Ready(_R) then
+			if Ready(_Q) and VMenu.ks.KSQ:Value() and (GetCurrentHP(unit)+GetDmgShield(unit)) < CalcDamage(myHero, unit, GetCastLevel(myHero,_Q) * 50 - 40 + GetBonusDmg(unit) * 1.5 , GetCastLevel(myHero,_R) * 75 + 25 + GetBonusAP(unit) + 3 * GetMaxHP(unit) * ((1.25 + GetCastLevel(myHero,_W) * 0.75) + GetBonusAP(unit) * 0.02) *.01) and GetCurrentHP(unit)+GetDmgShield(unit) > CalcDamage(myHero, unit, GetCastLevel(myHero,_Q) * 50 - 40 + GetBonusDmg(unit) * 1.5,GetMaxHP(unit) * ((1.25 + GetCastLevel(myHero,_W) * 0.75) + GetBonusAP(unit) * 0.02) * .01* wTrack[GetObjectName(unit)]) then
+				local RPred=GetPrediction(unit, VarusR)
+				local VarusQ = { delay = 0.1, speed = 1850, width = 70, range = qRange }
+				local QPred = GetPrediction(unit, VarusQ)
+				if RPred and RPred.hitChance >= (VMenu.p.hR:Value()/100) and QPred and QPred.hitChance >= (VMenu.p.hQ:Value()/100) then
+					CastSkillShot(_R,RPred.castPos)
+					DelayAction( function()
+						CastSkillShot(_Q,GetOrigin(myHero))	
+							DelayAction( function()
+								CastSkillShot2(_Q, GetOrigin(unit)) 
+							end,50)
+					end,50)
+				end
+			end
+		end
+	
+		--Q1
+		if VMenu.ks.KSQ:Value() and Ready(_Q) and ValidTarget(unit,1500) and GetCurrentHP(unit) + GetDmgShield(unit) <  CalcDamage(myHero, unit, GetCastLevel(myHero,_Q) * 50 - 40 + GetBonusDmg(unit) * 1.5 ,0) + StackDamage(unit) then 
 			if not qCharge then
 				CastSkillShot(_Q,GetOrigin(myHero))	
+		--Q2
 			elseif qCharge then
 				local VarusQ = { delay = 0.1, speed = 1850, width = 70, range = qRange }
 				local QPred = GetPrediction(unit, VarusQ)
@@ -148,12 +172,16 @@ function ks()
 					end
 			end
 		end
+		
+		--E
 		if VMenu.ks.KSE:Value() and Ready(_E) and ValidTarget(unit,GetCastRange(myHero,_E)) and GetCurrentHP(unit)+GetDmgShield(unit) <  CalcDamage(myHero, unit, GetCastLevel(myHero,_E)*35+30+GetBonusDmg(unit)*.6 ,0) + StackDamage(unit) then 
 			local EPred=GetCircularAOEPrediction(unit, VarusE)
 			if EPred and EPred.hitChance >= (VMenu.p.hE:Value()/100) then
 				CastSkillShot(_E,EPred.castPos)
 			end
 		end
+		
+		--R
 		if VMenu.ks.KSR:Value() and Ready(_R) and ValidTarget(unit,1075) and GetCurrentHP(unit)+GetDmgShield(unit) <  CalcDamage(myHero, unit, 0 ,GetCastLevel(myHero,_R)*75+25+GetBonusAP(unit)) then 
 			local RPred=GetPrediction(unit, VarusR)
 			if RPred and RPred.hitChance >= (VMenu.p.hR:Value()/100) then
@@ -164,8 +192,8 @@ function ks()
 end
 
 function StackDamage(unit)
-	if wTrack[unit] then
-		local wDmg = GetMaxHP(unit)*((1.25+GetCastLevel(myHero,_W)*0.75)+GetBonusAP(unit)*0.02)*.01*.95
+	if wTrack[GetObjectName(unit)] then
+		local wDmg = GetMaxHP(unit)*((1.25+GetCastLevel(myHero,_W)*0.75)+GetBonusAP(unit)*0.02)*.01
 		return CalcDamage(myHero, unit, 0, wDmg*wTrack[GetObjectName(unit)])
 	else
 		return 0
