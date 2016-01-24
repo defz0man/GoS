e2b810dcb9de1632fbb9d3e37a6496697f391271
LoggeL <hyper.xjo@gmx.de>
2016-01-24 18:43:01 +0100

    Updated everything

--- draw.lua
+++ draw.lua
@@ -1,45 +1,46 @@
 require('Inspired')
 require('DamageLib')
 
-dmgSpell={}
+local dmgSpell={0,0,0,0}
+local spellName= {"Q","W","E","R"} 
+local allDmg = 0
+local DrawMenu=Menu("Draw","Draw "..GetObjectName(myHero).." Damage")
+local dC = { {255,255,255,0}, {255,0,255,0}, {255,255,0,0}, {255,0,0,255} }
 for i=0,3,1 do
-	dmgSpell[i]=0
+	if getdmg(spellName[i+1],myHero,myHero,1,3)~=0 then
+		DrawMenu:Boolean(spellName[i+1], "Draw "..spellName[i+1], true)
+		DrawMenu:ColorPick(spellName[i+1].."c", "Color for "..spellName[i+1], dC[i+1])
+	end
 end
-allDmg=0
-
-spell={}
-spell[0]="Q"
-spell[1]="W"
-spell[2]="E"
-spell[3]="R"
-
-local DrawMenu=Menu("Draw","Draw")
-DrawMenu:SubMenu("c", GetObjectName(myHero))
-for i=0,3,1 do
-	DrawMenu.c:Boolean(spell[i], "Draw "..spell[i], true)
-end
-
-
 
 
 OnTick(function (myHero)
+	allDmg = 0 
 	for i=0,3,1 do
-		for _,i in pairs(spell) do
-			dmgSpell[i]=0
-			if DrawMenu.c.spell[i]:Value() and Ready(i) and ValidTarget(champ,GetCastRange(myHero,i)*2) and GetDistance(myHero,champ)<GetCastRange(myHero,i)*2 then
-				dmgSpell[i]= getdmg(spell[i],champ,myHero,GetCastLevel(myHero,i))
+		for _,champ in pairs(GetEnemyHeroes()) do
+		champ=myHero
+			dmgSpell[i+1]=0
+			if  DrawMenu[spellName[i+1]] and DrawMenu[spellName[i+1]]:Value() and Ready(i) and GetDistance(myHero,champ) < (GetCastRange(myHero,i)*2) then
+				dmgSpell[i+1] = getdmg(spellName[i+1],champ,myHero,GetCastLevel(myHero,i))
+			else 
+				dmgSpell[i+1] = 0
 			end
+			allDmg = allDmg + dmgSpell[i+1]
 		end
-		allDmg=dmgSpell[0]+dmgSpell[1]+dmgSpell[2]+dmgSpell[3]
 	end
 end)
 
 OnDraw(function (myHero)
+	champ=myHero
+	local cDmg = allDmg
 	for _,champ in pairs(GetEnemyHeroes()) do
-		if ValidTarget(champ) then
-			DrawDmgOverHpBar(champ,GetCurrentHP(champ),allDmg,0,0xffffffff)
+		for i=3,0,-1 do
+			if GetDistance(myHero,champ)< GetCastRange(myHero,i)*2 and DrawMenu[spellName[i+1].."c"] then
+				DrawDmgOverHpBar(champ,GetCurrentHP(champ),cDmg,0,DrawMenu[spellName[i+1].."c"]:Value())
+			end
+			cDmg=cDmg-dmgSpell[i+1]
 		end
 	end
 end)
 
-print("DmgDraw loaded - Logge")
+print("Hello "..GetUser().." :>")
