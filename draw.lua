74a5a74dde564b198c7c3840c48818ac3c52294f
LoggeL <hyper.xjo@gmx.de>
2016-01-26 19:09:31 +0100

    Update draw.lua
    Ta da

--- draw.lua
+++ draw.lua
@@ -1,44 +1,62 @@
 require('Inspired')
 require('DamageLib')
 
-local dmgSpell={0,0,0,0}
+local dmgSpell = {}
 local spellName= {"Q","W","E","R"} 
-local allDmg = 0
-local DrawMenu=Menu("Draw","Draw "..GetObjectName(myHero).." Damage")
 local dC = { {255,255,255,0}, {255,0,255,0}, {255,255,0,0}, {255,0,0,255} }
-for i=0,3,1 do
-	if getdmg(spellName[i+1],myHero,myHero,1,3)~=0 then
-		DrawMenu:Boolean(spellName[i+1], "Draw "..spellName[i+1], true)
-		DrawMenu:ColorPick(spellName[i+1].."c", "Color for "..spellName[i+1], dC[i+1])
+local aa = {}
+local dCheck = {}
+local DrawMenu=Menu("Draw","Draw "..GetObjectName(myHero).." Damage")
+DrawMenu:Boolean("dAA","Count AA to kill", true)
+DrawMenu:Boolean("dAAc","Consider Crit", true)
+
+
+for i=1,4,1 do
+	if getdmg(spellName[i],myHero,myHero,1,3)~=0 then
+		DrawMenu:Boolean(spellName[i], "Draw "..spellName[i], true)
+		DrawMenu:ColorPick(spellName[i].."c", "Color for "..spellName[i], dC[i])
 	end
 end
 
+DelayAction( function()
+	for _,champ in pairs(GetEnemyHeroes()) do
+		dmgSpell[GetObjectName(champ)]={0,0,0,0}
+	end
+end, 0.001)
 
 OnTick(function (myHero)
-	allDmg = 0 
-	for i=0,3,1 do
-		for _,champ in pairs(GetEnemyHeroes()) do
-		champ=myHero
-			dmgSpell[i+1]=0
-			if  DrawMenu[spellName[i+1]] and DrawMenu[spellName[i+1]]:Value() and Ready(i) and GetDistance(myHero,champ) < (GetCastRange(myHero,i)*2) then
-				dmgSpell[i+1] = getdmg(spellName[i+1],champ,myHero,GetCastLevel(myHero,i))
+	for _,champ in pairs(GetEnemyHeroes()) do
+		dCheck[GetObjectName(champ)]={false,false,false,false}
+		local sDmg = 0
+		for i=1,4,1 do
+			dmgSpell[GetObjectName(champ)][i]=0
+			if  DrawMenu[spellName[i]] and DrawMenu[spellName[i]]:Value() and Ready(i-1) and GetDistance(GetOrigin(myHero),GetOrigin(champ)) < (GetCastRange(myHero,i-1)*2) then
+				sDmg = sDmg + getdmg(spellName[i],champ,myHero,GetCastLevel(myHero,i-1))
+				dCheck[GetObjectName(champ)][i]=true
 			else 
-				dmgSpell[i+1] = 0
+				dmgSpell[GetObjectName(champ)][i] = 0
+				dCheck[GetObjectName(champ)][i]=false
 			end
-			allDmg = allDmg + dmgSpell[i+1]
+			dmgSpell[GetObjectName(champ)][i] = sDmg
+		end
+		if DrawMenu.dAA:Value() and DrawMenu.dAAc:Value() then 
+			aa[GetObjectName(champ)] = math.ceil(GetCurrentHP(champ)/(CalcDamage(myHero, champ, GetBaseDamage(myHero)+GetBonusDmg(myHero),0)*(GetCritChance(myHero)+1)))
+		elseif DrawMenu.dAA:Value() and not DrawMenu.dAAc:Value() then 
+			aa[GetObjectName(champ)] = math.ceil(GetCurrentHP(champ)/(CalcDamage(myHero, champ, GetBaseDamage(myHero)+GetBonusDmg(myHero),0)))
 		end
 	end
 end)
 
 OnDraw(function (myHero)
-	champ=myHero
-	local cDmg = allDmg
 	for _,champ in pairs(GetEnemyHeroes()) do
-		for i=3,0,-1 do
-			if GetDistance(myHero,champ)< GetCastRange(myHero,i)*2 and DrawMenu[spellName[i+1].."c"] then
-				DrawDmgOverHpBar(champ,GetCurrentHP(champ),cDmg,0,DrawMenu[spellName[i+1].."c"]:Value())
+		local bar = GetHPBarPos(champ)
+		for i=4,1,-1 do
+			if dCheck[GetObjectName(champ)] and dCheck[GetObjectName(champ)][i] then
+				DrawDmgOverHpBar(champ,GetCurrentHP(champ),dmgSpell[GetObjectName(champ)][i], 0, DrawMenu[spellName[i].."c"]:Value())
 			end
-			cDmg=cDmg-dmgSpell[i+1]
+		end
+		if DrawMenu.dAA:Value() and bar.x ~= 0 and bar.y ~= 0 then 
+			DrawText(aa[GetObjectName(champ)].." AA", 15, bar.x + 75, bar.y + 25, GoS.White)
 		end
 	end
 end)
