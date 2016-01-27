07f34e7aee78c3e5e567b112584305c003d1326a
LoggeL <hyper.xjo@gmx.de>
2016-01-27 22:07:57 +0100

    Create AutoIgnite.lua

---
+++ AutoIgnite.lua
@@ -0,0 +1,12 @@
+if not Ignite then return end
+
+local IMenu = Menu("Ignite","Ignite")
+IMenu:Boolean("i","Auto Ignite", true")
+
+if IMenu.i:Value() and IsReady(Ignite) then
+  for _,enemy in pairs(GetEnemyHeroes()) do
+   if 20*GetLevel(myHero)+50 > GetHP(enemy)+GetHPRegen(enemy)*3 and ValidTarget(enemy, 600) then
+    CastTargetSpell(enemy, Ignite)
+   end
+  end
+end
