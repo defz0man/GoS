d432193c965d2a56d0449ee997af719cff2fce5c
LoggeL <hyper.xjo@gmx.de>
2016-01-29 21:35:19 +0100

    Update AutoIgnite.lua
    Formatting all the way

--- AutoIgnite.lua
+++ AutoIgnite.lua
@@ -4,18 +4,19 @@ if GetCastName(myHero,SUMMONER_1) == "summonerdot" then
 	Ignite = SUMMONER_1
 elseif GetCastName(myHero,SUMMONER_2) == "summonerdot" then
 	Ignite = SUMMONER_2
-else return
+else
+	return
 end
 
 local IMenu = Menu("Ignite","Ignite")
 IMenu:Boolean("i","Auto Ignite", true)
 
 OnTick(function(myHero)
-if IMenu.i:Value() and IsReady(Ignite) then
-  for _,enemy in pairs(GetEnemyHeroes()) do
-   if 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetHPRegen(enemy)*3 and ValidTarget(enemy, 600) then
-    CastTargetSpell(enemy, Ignite)
-   end
-  end
-end
+	if IMenu.i:Value() and IsReady(Ignite) then
+ 		for _,enemy in pairs(GetEnemyHeroes()) do
+  			if 20*GetLevel(myHero)+50 > GetCurrentHP(enemy)+GetHPRegen(enemy)*3 and ValidTarget(enemy, 600) then
+				CastTargetSpell(enemy, Ignite)
+			end
+		end
+	end
 end)
