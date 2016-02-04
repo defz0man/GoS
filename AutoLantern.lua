fd623bcd96e2a00d5035db4523a0a6f9a9ebd2a5
LoggeL <hyper.xjo@gmx.de>
2016-02-04 19:15:29 +0100

    Create AutoLantern.lua
    Release

---
+++ AutoLantern.lua
@@ -0,0 +1,49 @@
+require('Inspired')
+
+for _,i in pairs(GetAllyHeroes()) do
+	if GetObjectName(i)=="Thresh" then
+		break
+	end
+	return
+end
+
+local lantern = nil
+local Lt = nil
+local LMenu = Menu("AL","AutoLantern")
+LMenu:Boolean("PL","AutoPick Lantern",true)
+LMenu:Boolean("PC","AutoPick in Combo",true)
+LMenu:Slider("PH", "AutoPick when lower than", 30, 0, 100, 1)
+
+OnTick(function(myHero)
+	if lantern and GetTeam(lantern) == MINION_ALLY and GetDistance(GetOrigin(lantern))<280 and LMenu.PL:Value() and (KeyIsDown(32) or not LMenu.PC:Value() or LMenu.PH:Value()>GetPercentHP(myHero)) then
+		Interact(lantern)
+	end
+	if Lt and GetTickCount()-Lt>5900 then
+	Lt = nil
+	end
+end)
+
+
+OnDraw(function(myHero)
+	if Lt and lantern then
+	local lS = WorldToScreen(0, GetOrigin(lantern))
+		DrawText(6-math.floor(GetTickCount()-Lt)*.001,30,lS.x,lS.y+10,GoS.White)
+	end
+end)
+
+
+OnCreateObj(function(Object)
+	if GetObjectBaseName(Object)=="ThreshLantern" then
+		Lt = GetTickCount()
+		lantern = Object
+		DelayAction(function() lantern = nil Lt = nil end, 6)
+	end
+end)
+
+OnDeleteObj(function(Object,myHero)
+	if GetObjectBaseName(Object)=="ThreshLantern" then 
+		lantern = nil
+	end
+end)
+
+print("AutoLantern loaded")
