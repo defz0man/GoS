ce39d0c3ae859e05020a332b7a846b6fc83c24ae
LoggeL <hyper.xjo@gmx.de>
2016-04-18 19:28:54 +0200

    Update Nasus.lua

--- Nasus.lua
+++ Nasus.lua
@@ -1,6 +1,5 @@
 if GetObjectName(GetMyHero()) ~= "Nasus" then return end
 
-require("Inspired")
 if not pcall( require, "OpenPredict" ) then PrintChat("This script doesn't work without OpenPredict! Download it!") return end
 
 local version = 1
